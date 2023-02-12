49bcdaf650fc7af1a26fe4fe14e748eb5111c57c
[PaskoEvgeniy] <[pasko.e.sms@gmail.com]>
2023-02-12 12:58:31 +0200

    Sozdal klass

---
+++ DomaZadaniya.cs
@@ -0,0 +1,8 @@
+public class DomaZadaniya
+{
+public double ID_Student {get;set;}
+public double ID_Prepod {get;set;}
+public string? Predmet {get; set;}
+public string? Opisaniya {get; set;}
+public double Ocenka {get; set;}
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Linq;
+
+
+var DomaZadaniya = new List<DomaZadaniya>()
+{
+new DomaZadaniya {ID_Student = 12345, ID_Prepod = 4321, Predmet = "Matematika", Opisaniya = "Rechit" ,Ocenka = 5},
+new DomaZadaniya 
+
+
+};
+
