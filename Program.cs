45986a7cf54ed0a664b97e661d572eb938521fe7
[PaskoEvgeniy] <[pasko.e.sms@gmail.com]>
2023-02-12 13:20:07 +0200

    Class DZ

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
 ﻿using System.Linq;
 
 
-var DomaZadaniya = new List<DomaZadaniya>()
+var zadaniya = new List<DomaZadaniya>()
 {
 new DomaZadaniya {ID_Student = 12345, ID_Prepod = 4321, Predmet = "Matematika", Opisaniya = "Rechit" ,Ocenka = 5},
-new DomaZadaniya 
-
+new DomaZadaniya {ID_Student = 14345, ID_Prepod = 4321, Predmet = "Trudi", Opisaniya = "Zdelati" ,Ocenka = 8},
+new DomaZadaniya {ID_Student = 12765, ID_Prepod = 4321, Predmet = "Mova", Opisaniya = "Napisat" ,Ocenka = 9},
 
 };
+
+foreach (var DomaZadaniya in zadaniya )
+Console.WriteLine($"ID_Student: {DomaZadaniya.ID_Student}, ID_Prepod: {DomaZadaniya.ID_Prepod}, Predmet:{DomaZadaniya.Predmet}, Opisaniya: {DomaZadaniya.Opisaniya}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
